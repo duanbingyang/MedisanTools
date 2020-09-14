9cccd07310d4e67b14c75eb30b4000adb316af65
duanbingyang <duanby0426@163.com>
2020-09-14 13:36:55 +0800

    'modify'

--- server/src/service/file-oss.js
+++ server/src/service/file-oss.js
@@ -3,8 +3,8 @@ const OSS = require('ali-oss');
 const client = new OSS({
     region: 'oss-cn-beijing',
     //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
-    accessKeyId: 'LTAIfShTeej7TjoZ',
-    accessKeySecret: 'd4oEKK2nAmpIyFdrsENmYDIzwk7k7b',
+    // accessKeyId: '',
+    // accessKeySecret: '',
     bucket: 'medisan'
 });
 
